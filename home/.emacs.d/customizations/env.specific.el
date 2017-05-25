c5c75ea1ee255ba604de73391d024f924918ce51
zetxx <zetxx@belogradcik.biz>
2017-05-25 21:47:44 +0300

    fix: merge

--- home/.emacs.d/customizations/env.specific.el
+++ home/.emacs.d/customizations/env.specific.el
@@ -1,3 +1,3 @@
-(defvar ternjsPath "path to ternjs bin, usualy @ ~/npm/lib/node_modules/tern/emacs/")
+(defvar ternjsPath "/home/zetxx/npm/lib/node_modules/tern/emacs/")
 (defvar globalFont "xos4 Terminus 10")
 (defvar esLintPath "~/dotfiles/tools/lints/rules/.eslintrc")
